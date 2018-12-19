3e7e0d0035313e08a22bef0b97f857651b31d6d3
lzt68 <870051555@qq.com>
2018-12-19 15:42:18 +0800

    登录网站测试了几次
    新加了其他几位同学的数据

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '419!=2ek^gp2-v(zeil_s7j6jn)a37mynr843a#flln1j#bkx$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
